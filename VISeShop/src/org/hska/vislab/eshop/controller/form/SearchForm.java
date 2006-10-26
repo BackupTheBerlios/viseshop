daafc7e974f3b600052a13f492ea99da6d2b6eae
maen0012 <maen0012>
2006-10-26 16:18:26 +0000

    validate methode implementiert

--- VISeShop/src/org/hska/vislab/eshop/controller/form/SearchForm.java
+++ VISeShop/src/org/hska/vislab/eshop/controller/form/SearchForm.java
@@ -3,31 +3,29 @@ package org.hska.vislab.eshop.controller.form;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.struts.action.ActionErrors;
-import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.action.ActionMessage;
+import org.apache.struts.validator.ValidatorForm;
 
-public class SearchForm extends ActionForm {
+public class SearchForm extends ValidatorForm {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 5310202539401178378L;
 
-	private String name = null;
-
-	private String description = null;
+	private String searchedtext = null;
 	
 	private String lowerprice = null;
 	
 	private String upperprice = null;
 
-	public String getDescription() {
-		return description;
+	public String getSearchedtext() {
+		return searchedtext;
 	}
 
-	public void setDescription(String description) {
-		this.description = description;
+	public void setSearchedtext(String searchedtext) {
+		this.searchedtext = searchedtext;
 	}
 
 	public String getLowerprice() {
@@ -38,13 +36,6 @@ public class SearchForm extends ActionForm {
 		this.lowerprice = lowerPrice;
 	}
 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
 
 	public String getUpperprice() {
 		return upperprice;
@@ -53,5 +44,22 @@ public class SearchForm extends ActionForm {
 	public void setUpperprice(String upperPrice) {
 		this.upperprice = upperPrice;
 	}
-
+	
+	public ActionErrors validate(ActionMapping mapping,
+			HttpServletRequest request) {
+		ActionErrors errors = new ActionErrors();
+		try{
+			if ( (upperprice != null) && (upperprice.length() > 0) )
+			Float.parseFloat(upperprice);
+		} catch (Exception ex){
+			errors.add("search", new ActionMessage("error.search.upperprice"));
+		}
+		try{
+			if ( (lowerprice != null) && (lowerprice.length() > 0) )
+			Float.parseFloat(lowerprice);
+		} catch (Exception ex){
+			errors.add("search", new ActionMessage("error.search.lowerprice"));
+		}
+		return errors;
+	}
 }
